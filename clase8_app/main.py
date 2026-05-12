3c6b1b523ef5a95a1b244ec8666ca36f0f245644
lusholh <luislushoh@gmail.com>
2026-05-12 07:47:24 -0600

    clase8:App de gráficos sobre países // Terminado

--- clase8_app/main.py
+++ clase8_app/main.py
@@ -4,30 +4,50 @@ import charts
 import pandas as pd
 
 def run():
-  '''
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  '''
+#  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+#  countries = list(map(lambda x: x['Country'], data))
 
-  df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'Africa']
+#  df = pd.read_csv('data.csv')
+#  df = df[df['Continent'] == 'Africa']
 
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  charts.generate_pie_chart(countries, percentages)
+#  countries = df['Country'].values
+#  percentages = df['World Population Percentage'].values
+#  charts.generate_pie_chart(countries, percentages)
+
+#  data = read_csv.read_csv('data.csv')
+#  country = input('Type Country => ')
+#  print(country)
+
+#  result = utils.population_by_country(data, country)
+
+#  if len(result) > 0:
+#    country = result[0]
+#    print(country)
+#    labels, values = utils.get_population(country)
+#    charts.generate_bar_chart(country['Country'], labels, values)
+
 
   data = read_csv.read_csv('data.csv')
+  #data = list(filter(lambda item : item['Continent'] == 'South America',data))
+    # APLICAR FILTRO DE SÓLO SOUTH AMERICA
+
+  countries = list(map(lambda x: x['Country'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
   country = input('Type Country => ')
   print(country)
 
   result = utils.population_by_country(data, country)
 
-  if len(result) > 0:
+  if(len(result) > 0):
     country = result[0]
     print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
+  else:
+    print('It seems like that country does not exist!')
+
 
 if __name__ == '__main__':
   run()
